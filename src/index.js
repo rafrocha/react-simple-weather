5e347d7a9d7dcbf4c53c0a725912c63518cbad66
Raf <rafaelmarianorocha@gmail.com>
2019-03-11 16:17:11 -0700

    Clear unused files and get latitude.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)
+        );
+    }
+    componentDidUpdate() {}
+    render() {
+        const { lat } = this.state;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        return <div>Latitude: {lat}</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
